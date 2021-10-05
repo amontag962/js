88e393a1f5426171604d2d79ecb5269d3932c71a
Alexander <amontag962@yandex.ru>
2021-10-05 22:40:45 +0300

    Выполнен урок 2

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fuulPrice;
-let adaptive;
+let title = "Авиасеилс";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 5000;
+let rollback = 95;
+let fuulPrice = 20000;
+let adaptive = !5;
 
-alert("welcome");
-
-console.log("this is text");
+console.log("Добрый день");
+console.log("title: ", typeof title);
+console.log("fuulPrice: ", typeof fuulPrice);
+console.log("adaptive: ", typeof adaptive);
+console.log("screens: ", screens.length);
+console.log("Стоимость верстки экранов: ", screenPrice, "рублей");
+console.log("стоимость разработки сайта: ", fuulPrice, "рублей");
+console.log("screens: ", screens.toLowerCase());
+console.log("screens: ", screens.split());
+console.log(fuulPrice * (rollback / 100));
+alert("Расчет стоимости услуг готов");
