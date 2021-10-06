7084c67d043ae96839024b7b650744c2885f63d2
amontag962 <amontag962@yandex.ru>
2021-10-06 14:34:21 +0300

    fix: добавлен массив в screens errors fixed

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ let title = "Авиасеилс";
 let screens = "Простые, Сложные, Интерактивные";
 let screenPrice = 5000;
 let rollback = 95;
-let fuulPrice = 20000;
+let fullPrice = 20000;
 let adaptive = !5;
+
+screens = screens.toLowerCase().split(",");
 
 console.log("Добрый день");
 console.log("title: ", typeof title);
-console.log("fuulPrice: ", typeof fuulPrice);
+console.log("fuulPrice: ", typeof fullPrice);
 console.log("adaptive: ", typeof adaptive);
 console.log("screens: ", screens.length);
 console.log("Стоимость верстки экранов: ", screenPrice, "рублей");
-console.log("стоимость разработки сайта: ", fuulPrice, "рублей");
-console.log("screens: ", screens.toLowerCase().split());
-console.log(fuulPrice * (rollback / 100));
-// alert("Расчет стоимости услуг готов");
+console.log("стоимость разработки сайта: ", fullPrice, "рублей");
+console.log("screens: ", screens);
+console.log(fullPrice * (rollback / 100));
+alert("Расчет стоимости услуг готов");
