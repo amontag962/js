5ab355b4fd98c82c3b3b0e84d98599ab1d0e38cb
Alexander <amontag962@yandex.ru>
2021-10-11 21:27:09 +0300

    Сделал урок 3

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-let title = "Авиасеилс";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 5000;
-let rollback = 95;
-let fullPrice = 20000;
-let adaptive = !5;
+// Переменные
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать?");
+let screenPrice = +prompt("Сколько будет стоить данная работа?");
+let rollback = +prompt("Сколько будут стоить услуги посредника?");
+let adaptive = confirm("Нужен ли адаптив на сайте");
+let questionOne = prompt("Какой дополнительный тип услуги нужен?");
+let priceOne = +prompt("Сколько это будет стоить?");
+let questionTwo = prompt("Какой дополнительный тип услуги нужен?");
+let priceTwo = +prompt("Сколько это будет стоить?");
+let fullPrice = screenPrice + priceOne + priceTwo;
+let servicePercentPrice = Math.ceil(fullPrice - rollback);
 
-screens = screens.toLowerCase().split(",");
+// Вывод данных в консоль
+console.log("servicePercentPrice: ", servicePercentPrice);
+screens = screens.toLowerCase().split(" ");
 
 console.log("Добрый день");
-console.log("title: ", typeof title);
-console.log("fuulPrice: ", typeof fullPrice);
-console.log("adaptive: ", typeof adaptive);
-console.log("screens: ", screens.length);
 console.log("Стоимость верстки экранов: ", screenPrice, "рублей");
 console.log("стоимость разработки сайта: ", fullPrice, "рублей");
 console.log("screens: ", screens);
-console.log(fullPrice * (rollback / 100));
 alert("Расчет стоимости услуг готов");
+
+if (fullPrice > 30000) {
+  console.log("Ваша скидка 10%");
+} else if (30000 >= fullPrice > 15000) {
+  console.log("Ваша скидка 5%");
+} else if (15000 >= fullPrice >= 0) {
+  console.log("Скидка не предусмотрена");
+} else if (fullPrice < 0) {
+  console.log("Что то пошло не так");
+}
