b4777f6e1f8b962c3b562bbf48dd6d8d77c5b020
Alexander <amontag962@yandex.ru>
2021-10-11 22:02:45 +0300

    исправлена ошибка

--- script.js
+++ script.js
@@ -3,14 +3,15 @@
 let title = prompt("Как называется ваш проект?");
 let screens = prompt("Какие типы экранов нужно разработать?");
 let screenPrice = +prompt("Сколько будет стоить данная работа?");
-let rollback = +prompt("Сколько будут стоить услуги посредника?");
+let rollback = 95;
+let support = +prompt("Сколько будут стоить услуги посредника?");
 let adaptive = confirm("Нужен ли адаптив на сайте");
 let questionOne = prompt("Какой дополнительный тип услуги нужен?");
 let priceOne = +prompt("Сколько это будет стоить?");
 let questionTwo = prompt("Какой дополнительный тип услуги нужен?");
 let priceTwo = +prompt("Сколько это будет стоить?");
 let fullPrice = screenPrice + priceOne + priceTwo;
-let servicePercentPrice = Math.ceil(fullPrice - rollback);
+let servicePercentPrice = Math.ceil(fullPrice - support);
 
 // Вывод данных в консоль
 console.log("servicePercentPrice: ", servicePercentPrice);
@@ -20,9 +21,10 @@ console.log("Добрый день");
 console.log("Стоимость верстки экранов: ", screenPrice, "рублей");
 console.log("стоимость разработки сайта: ", fullPrice, "рублей");
 console.log("screens: ", screens);
+console.log(fullPrice * (rollback / 100));
 alert("Расчет стоимости услуг готов");
 
-if (fullPrice > 30000) {
+if (fullPrice >= 30000) {
   console.log("Ваша скидка 10%");
 } else if (30000 >= fullPrice > 15000) {
   console.log("Ваша скидка 5%");
