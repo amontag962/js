6f9f5707fcf875715831e1b64cacdf6080f6691d
Alexander <amontag962@yandex.ru>
2021-10-13 05:50:09 +0300

    выполнил задание 4

--- script.js
+++ script.js
@@ -1,33 +1,66 @@
 "use strict";
-// Переменные
+
+// Блок объявления переменных
 let title = prompt("Как называется ваш проект?");
 let screens = prompt("Какие типы экранов нужно разработать?");
 let screenPrice = +prompt("Сколько будет стоить данная работа?");
-let rollback = 10;
 let adaptive = confirm("Нужен ли адаптив на сайте");
 let questionOne = prompt("Какой дополнительный тип услуги нужен?");
 let priceOne = +prompt("Сколько это будет стоить?");
 let questionTwo = prompt("Какой дополнительный тип услуги нужен?");
 let priceTwo = +prompt("Сколько это будет стоить?");
+let rollback = 10;
 let fullPrice = screenPrice + priceOne + priceTwo;
 let servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100));
 
-// Вывод данных в консоль
-console.log("servicePercentPrice: ", servicePercentPrice);
-screens = screens.toLowerCase().split(" ");
-console.log("Добрый день");
-console.log("Стоимость верстки экранов: ", screenPrice, "рублей");
-console.log("стоимость разработки сайта: ", fullPrice, "рублей");
-console.log("screens: ", screens);
-alert("Расчет стоимости услуг готов");
-
-if (fullPrice >= 30000) {
-  console.log("Ваша скидка 10%");
-} else if (30000 > fullPrice && fullPrice > 15000) {
-  console.log("Ваша скидка 5%");
-} else if (15000 >= fullPrice && fullPrice >= 0) {
-  console.log("Скидка не предусмотрена");
-} else if (fullPrice < 0) {
-  console.log("Что то пошло не так");
+// Блок описания функций
+const getRollbackMessage = function (price) {
+  if (price >= 30000) {
+    return "Ваша скидка 10%";
+  } else if (30000 > price && price > 15000) {
+    return "Ваша скидка 5%";
+  } else if (15000 >= price && price >= 0) {
+    return "Скидка не предусмотрена";
+  } else if (price < 0) {
+    return "Что то пошло не так";
+  }
+};
+const showTypeOf = function (variable) {
+  console.log(variable, typeof variable);
+};
+const getAllServicePrices = function () {
+  const getSum = function (a, b) {
+    return a + b;
+  };
+};
+function getFullPrice() {
+  function getSum(a, b) {
+    return a + b;
+  }
 }
-// new lesson
+function getTitle() {
+  title = title.trim();
+  title = title[0].toUpperCase() + title.substring(1).toLowerCase();
+}
+function getServicePercentPrices() {
+  Math.ceil(fullPrice - fullPrice * (rollback / 100));
+}
+
+// Блок функционала
+screens = screens.toLowerCase().split(" ");
+let allServicePrices = getAllServicePrices(priceOne, priceTwo);
+fullPrice = getFullPrice(screenPrice, allServicePrices);
+
+getTitle();
+servicePercentPrice = getServicePercentPrices();
+
+// Блок выводов в консоль и логов (Треш блок)
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
+
+console.log(getRollbackMessage(fullPrice));
+console.log(screens.split(" "));
+console.log("servicePercentPrice: ", servicePercentPrice);
+
+alert("Расчет стоимости услуг готов");
