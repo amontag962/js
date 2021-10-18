c4782f485e7620993d4f714a15aeb96de442476f
Alexander <amontag962@yandex.ru>
2021-10-18 21:24:08 +0300

    изменил блок с переменными и поправил ошибки

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 "use strict";
 
-// Блок объявления переменных
-let title = prompt("Как называется ваш проект?");
-let screens = prompt("Какие типы экранов нужно разработать?");
-let screenPrice = +prompt("Сколько будет стоить данная работа?");
-let adaptive = confirm("Нужен ли адаптив на сайте");
-let questionOne = prompt("Какой дополнительный тип услуги нужен?");
-let priceOne = +prompt("Сколько это будет стоить?");
-let questionTwo = prompt("Какой дополнительный тип услуги нужен?");
-let priceTwo = +prompt("Сколько это будет стоить?");
-let rollback = 10;
-let fullPrice = screenPrice + priceOne + priceTwo;
-let servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100));
+// Блок объявления переменных----------------------------------------------------
 
-// Блок описания функций
+let title;
+let screens;
+let screenPrice;
+let adaptive;
+
+let questionOne = prompt("Какой дополнительный тип услуги нужен?", "от");
+let priceOne = +prompt("Сколько это будет стоить?", "3000");
+let questionTwo = prompt("Какой дополнительный тип услуги нужен?", "вал");
+let priceTwo = +prompt("Сколько это будет стоить?", "2000");
+
+let rollback = 10;
+let allServicePrices;
+let fullPrice;
+let servicePercentPrice;
+
+// Блок описания функций---------------------------------------------------------
+
+const asking = function () {
+  title = prompt("Как называется ваш проект?", "Калькулятор верстки");
+  screens = prompt("Какие типы экранов нужно разработать?", "большие");
+  screenPrice = +prompt("Сколько будет стоить данная работа?", "9000");
+  adaptive = confirm("Нужен ли адаптив на сайте");
+};
+// Задает вопросы пользователю
+
 const getRollbackMessage = function (fullPrice) {
   if (fullPrice >= 30000) {
     return "Ваша скидка 10%";
@@ -25,30 +38,41 @@ const getRollbackMessage = function (fullPrice) {
     return "Что то пошло не так";
   }
 };
+// Сумма скидки
+
 const showTypeOf = function (variable) {
   console.log(variable, typeof variable);
 };
+// Выводит в консоль типы данных
+
 const getAllServicePrices = function () {
   return priceOne + priceTwo;
 };
-function getFullPrice() {
-  return screenPrice + allServicePrices;
+// Сумма дополнительных услуг
+
+function getFullPrice(a, b) {
+  return a + b;
 }
+// Получаем сумму цен за экраны и доп услуг
+
 function getTitle() {
   title = title.trim();
   title = title[0].toUpperCase() + title.substring(1).toLowerCase();
 }
+// Функция добавления верхнего регистра для первой буквы
+
 function getServicePercentPrices() {
   return Math.ceil(fullPrice - fullPrice * (rollback / 100));
 }
+// Получаем сумму с учетом скидки
 
-// Блок функционала
+// Блок функционала--------------------------------------------------------------
+asking();
 screens = screens.toLowerCase().split(" ");
-let allServicePrices = getAllServicePrices();
-fullPrice = getFullPrice();
-
-getTitle();
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice(screenPrice, allServicePrices);
 servicePercentPrice = getServicePercentPrices();
+title = getTitle();
 
 // Блок выводов в консоль и логов (Треш блок)
 showTypeOf(title);
@@ -60,5 +84,3 @@ console.log(screens);
 console.log("servicePercentPrice: ", servicePercentPrice);
 
 alert("Расчет стоимости услуг готов");
-
-//
